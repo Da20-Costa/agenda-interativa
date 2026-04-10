72ad5b3fd0677c2d6ae6eb26e58832e3142223e7
Davi Costa <da20.sousacosta@gmail.com>
2026-04-10 15:46:04 -0300

    test: add limit tests and tests with invalid inputs

--- tests/api/v1/compromissos/post.compromissos.test.js
+++ tests/api/v1/compromissos/post.compromissos.test.js
@@ -17,6 +17,46 @@ test('POST para /api/v1/compromissos deve criar um compromisso', async () => {
   expect(response.status).toBe(201)
 })
 
+test('POST para /api/v1/compromissos deve retornar erro 400 se o título tiver mais de 100 caracteres', async () => {
+  const tituloGigante = 'A'.repeat(101)
+
+  const response = await fetch('http://localhost:3000/api/v1/compromissos', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      titulo: tituloGigante,
+      data: 'Quinta',
+      descricao: 'Teste de limite de caracteres',
+    }),
+  })
+
+  const responseBody = await response.json()
+
+  expect(response.status).toBe(400)
+  expect(responseBody.erro).toBe(
+    'O título é obrigatório e deve ter no máximo 100 caracteres.',
+  )
+})
+
+test('POST para /api/v1/compromissos deve retornar erro 400 se a data for inválida', async () => {
+  const response = await fetch('http://localhost:3000/api/v1/compromissos', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      titulo: 'Reunião de Teste',
+      data: 'dataInvalida',
+      descricao: 'Teste de validação de data',
+    }),
+  })
+
+  const responseBody = await response.json()
+
+  expect(response.status).toBe(400)
+  expect(responseBody.erro).toBe(
+    'Data inválida. Use um dia da semana válido (ex: Segunda, Terça...).',
+  )
+})
+
 afterAll(() => {
   const stmt = db.prepare(
     "DELETE FROM compromissos WHERE titulo = 'Consulta médica'",
